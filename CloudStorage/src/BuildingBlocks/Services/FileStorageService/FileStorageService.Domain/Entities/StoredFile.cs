acaf406ec42e5b559569d93b907c051c34a7b741
emingarip <emin@emingarip.com>
2025-05-14 13:38:48 +0300

    Add Entities , Events , Interfaces in FileStorageService Domain Layer under FileStorageService

---
+++ CloudStorage/src/BuildingBlocks/Services/FileStorageService/FileStorageService.Domain/Entities/StoredFile.cs
@@ -0,0 +1,47 @@
+﻿using FileStorageService.Domain.Events;
+using SharedKernel;
+
+namespace FileStorageService.Domain.Entities;
+
+public class StoredFile : BaseEntity
+{
+    public Guid FileId { get; private set; }
+    public string FileName { get; private set; }
+    public string ContentType { get; private set; }
+    public long Size { get; private set; }
+    public string Path { get; private set; }
+    public Guid OwnerId { get; private set; }
+    public bool IsDeleted { get; private set; }
+
+    // For EF Core
+    private StoredFile() { }
+
+    public StoredFile(Guid fileId, string fileName, string contentType, long size, string path, Guid ownerId)
+    {
+        FileId = fileId;
+        FileName = fileName;
+        ContentType = contentType;
+        Size = size;
+        Path = path;
+        OwnerId = ownerId;
+        IsDeleted = false;
+
+        AddDomainEvent(new FileStoredEvent(Id, FileId, FileName, OwnerId));
+    }
+
+    public void Delete()
+    {
+        IsDeleted = true;
+        MarkAsUpdated();
+
+        AddDomainEvent(new FileDeletedEvent(Id, FileId, OwnerId));
+    }
+
+    public void Restore()
+    {
+        IsDeleted = false;
+        MarkAsUpdated();
+
+        AddDomainEvent(new FileRestoredEvent(Id, FileId, OwnerId));
+    }
+}
---
+++ CloudStorage/src/BuildingBlocks/Services/FileStorageService/FileStorageService.Domain/Interfaces/IFileStorageProvider.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using SharedKernel;
+
+namespace FileStorageService.Domain.Interfaces
+{
+    public interface IFileStorageProvider
+    {
+        Task<Result<(string path, long size)>> StoreFileAsync(IFormFile file, Guid fileId, Guid ownerId);
+        Task<Result<string>> GetFileUrlAsync(string path);
+        Task<Result<string>> GetDownloadUrlAsync(string path, string fileName);
+        Task<Result<Stream>> GetFileStreamAsync(string path);
+        Task<Result> DeleteFileAsync(string path);
+    }
+}
